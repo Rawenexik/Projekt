44209d6309eaa9ca2761cf8af53da443c92582ee
Jakub Komenda <47889131+Rawenexik@users.noreply.github.com>
2020-11-29 22:11:20 +0100

    Dnesni uprava

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-const char *ssid = "name"; // Enter your WiFi name
-const char *password = "pass";  // Enter WiFi password
-const char *mqtt_broker = "broker.emqx.io";
+const char *ssid = "SAO82"; // Enter your WiFi name
+const char *password = "61122406";  // Enter WiFi password
+const char *mqtt_broker = "192.168.1.253";
 const int mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+}
 
 void setup() {
     // Set software serial baud to 115200;
@@ -37,17 +48,9 @@ void setup() {
     client.subscribe("esp8266/test");
 }
 
-void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
-    }
-    Serial.println();
-    Serial.println("-----------------------");
-}
+
 
 void loop() {
     client.loop();
+    client.publish("teplota","33");
 }
