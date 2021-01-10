7e30b315f53e9d7c0575f9982997d7b9eb308093
Jakub Komenda <47889131+Rawenexik@users.noreply.github.com>
2021-01-10 17:00:55 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # Závěrečný projekt
-Měření teploty pomoci thermistoru,tyto hodnoty se budou zobrazovat na vytvořeném webovém serveru(Home Assistant) skrz MQTT. 
+Měření teploty pomoci thermistoru,tyto hodnoty se budou zobrazovat v grafu na vytvořeném webovém serveru(Home Assistant) skrz MQTT. 
 
 # Součástky
 1x ESP8266
 1x Thermistor
+1x Rezistor 10k O
+
+# Pracovní plán
+1. objednání hardwaru - HOTOVO
+2. poskládání hardwaru - HOTOVO
+3. psaní kodu a snažení se vypsat hodnoty do webového serveru skrz MQTT - HOTOVO
+4. zkouška, jestli funguje - HOTOVO
+5. kontrola chyb, popřípadě opravení - KONTROLA DROBNÝCH CHYB
+6. dokončení - HOTOVO
 
 # Zdroje
 https://www.electronicwings.com/nodemcu/thermistor-interfacing-with-nodemcu
 https://navody.arduino-shop.cz/arduino-projekty/mereni-teploty-s-termistorem.html
 https://iotdesignpro.com/projects/how-to-connect-esp8266-with-mqtt
-https://www.youtube.com/watch?v=sVqyDtEjudk&ab_channel=TheHookUp
-
-# Pracovní plán
-1. objednání hardwaru - HOTOVO
-2. poskládání hardwaru - HOTOVO
-3. psaní kodu a snažení se vypsat hodnoty do webového serveru skrz MQTT - PRACUJU NA TOM
-4. zkouška, jestli funguje - NEMAM
-5. kontrola chyb, popřípadě opravení - NEMAM
-6. dokončení - NEMAM
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,151 @@
+/*
+ Basic ESP8266 MQTT example
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+*/
+
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-const char *ssid = "SAO82"; // Enter your WiFi name
-const char *password = "61122406";  // Enter WiFi password
-const char *mqtt_broker = "192.168.1.253";
-const int mqtt_port = 1883;
+
+// Update these with values suitable for your network.
+
+const char* ssid = "SAO82";
+const char* password = "61122406";
+
+const char* mqtt_server = "192.168.1.115";
+
+const double VCC = 3.3;             
+const double R2 = 10000;            
+const double adc_resolution = 1023; 
+
+const double A = 0.001129148;   
+const double B = 0.000234125;
+const double C = 0.0000000876741; 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
 
-void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+                                                     // client.publish("zprava", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
-    Serial.println();
-    Serial.println("-----------------------");
+  }
 }
 
 void setup() {
-    // Set software serial baud to 115200;
-    Serial.begin(115200);
-    // connecting to a WiFi network
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.println("Connecting to WiFi..");
-    }
-    Serial.println("Connected to the WiFi network");
-    //connecting to a mqtt broker
-    client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(callback);
-    while (!client.connected()) {
-        Serial.println("Connecting to public emqx mqtt broker.....");
-        if (client.connect("esp8266-client")) {
-            Serial.println("Public emqx mqtt broker connected");
-        } else {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
-            delay(2000);
-        }
-    }
-    // publish and subscribe
-    client.publish("esp8266/test", "hello emqx");
-    client.subscribe("esp8266/test");
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
-
-
 void loop() {
-    client.loop();
-    client.publish("teplota","33");
+
+  double Vout, Rth, teplota, adc_value; 
+
+  adc_value = analogRead(A0);
+  Vout = (adc_value * VCC) / adc_resolution;
+  Rth = (VCC * R2 / Vout) - R2;
+
+  teplota = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)),3))));   
+
+  teplota = teplota - 273.15;  
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, " %lf", teplota);
+    Serial.println(teplota);
+    //Serial.println(msg);
+    client.publish("/testtopic/teplota", msg);
+  }
+  delay(500);
 }
